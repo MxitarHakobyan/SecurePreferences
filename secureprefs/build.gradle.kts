8b8297662f29c4f95b6b7a7226e879ebcca676f8
Mkhitar <hmxo14@gmail.com>
2025-01-23 17:53:50 +0400

    Updating gradle file

--- secureprefs/build.gradle.kts
+++ secureprefs/build.gradle.kts
@@ -1,10 +1,8 @@
-import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    id("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.28.0" apply false
+    id("com.gradleup.nmcp") version "0.0.7" apply false
 }
 
 android {
@@ -40,67 +38,4 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     testImplementation(libs.mockk)
     testImplementation(libs.junit)
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("release") {
-            artifact(file("${buildDir}/outputs/aar/${project.name}-release.aar"))
-
-            groupId = "com.github.MxitarHakobyan"
-            artifactId = "securepreferences"
-            version = "1.0.1"
-
-            pom {
-                name.set("SecurePreferences")
-                description.set("A secure preferences library for Android.")
-                url.set("https://github.com/MxitarHakobyan/SecurePreferences")
-
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("MxitarHakobyan")
-                        name.set("Mkhitar Hakobyan")
-                        email.set("hmxo14@gmail.com")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:https://github.com/MxitarHakobyan/SecurePreferences.git")
-                    developerConnection.set("scm:git:ssh://git@github.com/MxitarHakobyan/SecurePreferences.git")
-                    url.set("https://github.com/MxitarHakobyan/SecurePreferences")
-                }
-            }
-        }
-    }
-    val properties = gradleLocalProperties(rootDir, providers)
-    repositories {
-        maven {
-            name = "SecurePreferences"
-            url = uri("https://maven.pkg.github.com/MxitarHakobyan/SecurePreferences")
-
-            credentials {
-                username = System.getenv("GH_USER") ?: properties.getProperty("GH_USER")
-                password = System.getenv("GH_TOKEN") ?: properties.getProperty("GH_TOKEN")
-            }
-        }
-    }
-}
-
-tasks.withType(Test::class) {
-    testLogging {
-        events("skipped", "failed", "passed")
-        exceptionFormat = TestExceptionFormat.FULL
-        showStandardStreams = true
-    }
-}
-
-tasks.named("publishReleasePublicationToSecurePreferencesRepository") {
-    dependsOn("assembleRelease")
 }
